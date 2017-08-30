9b554acc8b49edc471208fa0869f3c1a238c15d1
ShrayankM <shraymist@gmail.com>
2017-08-30 18:47:25 +0530

    Add files via upload

---
+++ AS7.sql
@@ -0,0 +1,38 @@
+
+declare
+    roll_numberI number(5);
+    RollNumber number(5);
+    no_of_days number(5);
+    no_of_daysT number(5);
+    Name_of_Book varchar2(20);
+    BookName varchar2(20);
+    datet date;
+    fineV number(5);
+    fine number(5);
+    dateVar date := to_date(sysdate);
+
+begin
+
+    roll_numberI := &RollNumber;
+    Name_of_Book := &BookName;
+    fineV := &fine;
+
+    select DATEOFISSUE into datet from BorrowerN where ROLLIN = roll_numberI
+        and NAMEOFBOOK = Name_of_Book;
+
+        no_of_days := dateVar - datet;
+        dbms_output.put_line(no_of_days);
+    if(no_of_days > 15) then
+      fineV := (no_of_days - 15) * 15;
+    end if;
+
+    if(no_of_days > 30) then
+      no_of_daysT := no_of_days;
+      no_of_days := no_of_days - 30;
+      fineV := (no_of_days) * 50;
+      fineV := fineV + (15*5);
+    end if;
+
+
+    insert into FineN values(roll_numberI,dateVar,fineV);
+end;
